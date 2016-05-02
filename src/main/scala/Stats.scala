44b85c1be7ce49c96126bfb6930d951b2a271f06
ngsmithhotmailcom <ngsmith@hotmail.com>
2016-05-01 22:08:47 -0700

    updated pivot strategy

--- src/main/scala/Stats.scala
+++ src/main/scala/Stats.scala
@@ -15,88 +15,135 @@ import scala.collection.mutable._
 object MyApp extends App {
 
   object Median {
-
-    case class DataNode(isSorted: Boolean, data: Array[Int])
-
     import scala.collection.mutable._
 
-    def partitionOnEq(k: Int, data: Array[Int]): ArrayBuffer[DataNode] = {
+    def partitionOnEq(k: Int, data: Array[Int]): (Array[Int], Array[Int], Array[Int]) = {
       data match {
-        case v if (v.isEmpty) => new ArrayBuffer[DataNode]
-        case v if (!v.isEmpty) => {
-          val res = data.foldLeft((ArrayBuffer.empty[Int], k +: ArrayBuffer.empty[Int], ArrayBuffer.empty[Int])) {
+        case v if (v.isEmpty) => (Array.empty[Int], Array.empty[Int], Array.empty[Int])
+        case v => {
+          val res = data.foldLeft((ArrayBuffer.empty[Int], ArrayBuffer.empty[Int], ArrayBuffer.empty[Int])) {
             case ((acl:ArrayBuffer[Int], ace:ArrayBuffer[Int], acg:ArrayBuffer[Int]), i) => {
-              if (i == k)       ace.append(i)
-              else if (i < k)   acl.append(i)
-              else              acg.append(i)
-              (acl, ace, acg)
+              if (i == k)       (acl, ace :+ i, acg)
+              else if (i < k)   (acl :+ i, ace, acg)
+              else              (acl, ace, acg :+ i)
             }}
-          var buf = ArrayBuffer.empty[DataNode]
-          if (!res._1.isEmpty)
-            buf.append(new DataNode(res._1.length < 2, res._1.toArray))
-          if (!res._2.isEmpty)
-            buf.append(new DataNode(true, res._2.toArray))
-          if (!res._3.isEmpty)
-            buf.append(new DataNode(res._3.length < 2, res._3.toArray))
-          buf
+          ( res._1.toArray, res._2.toArray, res._3.toArray )
         }
       }
     }
 
-    def getMedianIndexNode(medianIndex:Int, nodes:ArrayBuffer[DataNode]): Int = {
-
-      val datalen = nodes.length
-      if (datalen <= 0) { println("getMedianNodeIndex ERROR: no data nodes."); -1 } else {
-        var aggLen = 0//nodes(0).data.length
-        var index = 0
-        while (index < datalen && aggLen -1 < medianIndex) {
-          val nodelen = nodes(index).data.length
-          aggLen += nodelen
-          index += 1
-        }
-        index -1
-      }
-    }
-
-    def median(init: DataNode): Int = {
-
+    def median(initData: Array[Int]): Int = {
+      //println(s"initData: ${initData.toList}")
       var err = 0
-
+      var pivotLB = Int.MinValue
+      var pivotUB = Int.MaxValue
       val MedianValues = new ArrayBuffer[Int]
-      val MedianIndexes = new ArrayBuffer[Int]
-      MedianIndexes += init.data.length/2
-      if (init.data.length%2 == 0) MedianIndexes.prepend(init.data.length/2 -1)
+      var MedianIndexes = new ArrayBuffer[Int]
+      MedianIndexes += initData.length/2
+      if (initData.length%2 == 0) MedianIndexes.prepend(initData.length/2 -1)
+      //println(s"MedianIndexes: ${MedianIndexes.toList}")
 
-      var nodes = new ArrayBuffer[DataNode]
-      nodes += init
+      var pivotCurr = initData.head
+      //println(s"pivotCurr: ${pivotCurr}")
+      var nodes = partitionOnEq(initData.head, initData)
+      //println(s"nodes: ${nodes._1.toList}\n${nodes._2.toList}\n${nodes._3.toList}")
 
       while (err == 0 && !MedianIndexes.isEmpty) {
-
-        val nodeIndex = getMedianIndexNode(MedianIndexes.head, nodes)
-        if (nodeIndex < 0) err = -1 else {
-
-          var nodesSplit = nodes.splitAt(nodeIndex)
-
-          if (nodesSplit._2.isEmpty) err = -2 else {
-            val pNode = nodesSplit._2.head
-            val pUpper = nodesSplit._2.tail
-
-            if (pNode.isSorted) {
-
-              val nodeSubIndex = nodesSplit._1.foldLeft(0)((ac, nd) => ac +nd.data.length)
-
-              val subindex = MedianIndexes.head -nodeSubIndex
-
-              MedianValues += pNode.data(subindex)
-
+        err = nodes match {
+          case ( _, pivoted, _) if (pivoted.isEmpty) => { println("ERROR: empty pivot column."); -1}
+          case (lower, pivoted, upper) =>
+            //println(s"nodes: \n${nodes._1.toList}\n${nodes._2.toList}\n${nodes._3.toList}")
+            //BOTH INDEXES IN LOWER SEGMENT
+            if (0 <= MedianIndexes.head && MedianIndexes.last < lower.length) {
+              //println
+              //println("BOTH INDEXES IN LOWER SEGMENT")
+              pivotUB = pivotCurr
+              //println(s"pivotUB: ${pivotUB}")
+              lower.find(e => pivotLB < e && e < pivotUB) match {
+                case None => { println("ERROR: bad pivot 0."); -2 }
+                case Some(pivot:Int) => {
+                  //println(s"pivot: ${pivot}")
+                  pivotCurr = pivot
+                  nodes = partitionOnEq(pivotCurr, lower)
+                  0
+                }
+              }
+            }
+            //HIGHER INDEX IN PIVOTED SEGMENT
+            else if (0 <= MedianIndexes.head && MedianIndexes.head < lower.length) {
+              //println
+              //println("HIGHER INDEX IN PIVOTED SEGMENT")
+              if (1 < MedianIndexes.length) {
+                MedianValues += pivoted.head
+                MedianIndexes.remove(1)
+              }
+              pivotUB = pivotCurr
+              //println(s"pivotUB: ${pivotUB}")
+              lower.find(e => pivotLB < e && e < pivotUB) match {
+                case None => { println("ERROR: bad pivot 1."); -2 }
+                case Some(pivot:Int) => {
+                  pivotCurr = pivot
+                  nodes = partitionOnEq(pivotCurr, lower)
+                  0
+                }
+              }
+            }
+            //BOTH INDICES IN PIVOTED SEGMENT
+            else if (lower.length <= MedianIndexes.head && MedianIndexes.last < (lower.length +pivoted.length)) {
+              //println
+              //println("BOTH INDICES IN PIVOTED SEGMENT")
+              MedianValues += pivoted.head
               MedianIndexes.remove(0)
+              if (0 < MedianIndexes.length) {
+                MedianValues += pivoted.head
+                MedianIndexes.remove(0)
+              }
+              0
+            }
+            //LOWEST INDEX IN PIVOTED, HIGHER INDEX NOT
+            else if (lower.length <= MedianIndexes.head && MedianIndexes.head < (lower.length +pivoted.length)) {
+              //println
+              //println("LOWEST INDEX IN PIVOTED, HIGHER INDEX NOT")
+              MedianValues += pivoted.head
+              MedianIndexes.remove(0)
+              if (0 < MedianIndexes.length) {
+                pivotLB = pivotCurr
+                //println(s"pivotLB: ${pivotLB}")
+                MedianIndexes = MedianIndexes.map(index => index -(lower.length +pivoted.length))
+                upper.find(e => pivotLB < e && e < pivotUB) match {
+                  case None => { println("ERROR: bad pivot 2."); -2 }
+                  case Some(pivot:Int) => {
+                    pivotCurr = pivot
+                    nodes = partitionOnEq(pivotCurr, upper)
+                  }
+                }
+              }
+              0
+            }
+            //BOTH INDEXES IN UPPER SEGMENT
+            else if ((lower.length +pivoted.length) <= MedianIndexes.head && MedianIndexes.last < (lower.length +pivoted.length +upper.length)) {
+              //println
+              //println("BOTH INDEXES IN UPPER SEGMENT")
+              pivotLB = pivotCurr
+              //println(s"pivotLB: ${pivotLB}")
+              MedianIndexes = MedianIndexes.map(index => index -(lower.length +pivoted.length))
+              upper.find(e => pivotLB < e && e < pivotUB) match {
+                case None => { println("ERROR: bad pivot 3."); -2 }
+                case Some(pivot:Int) => {
+                  pivotCurr = pivot
+                  nodes = partitionOnEq(pivotCurr, upper)
+                }
+              }
+              0
             }
             else {
-              val parts = partitionOnEq(pNode.data.head, pNode.data.tail)
-              nodes = nodesSplit._1 ++ parts ++ pUpper
-            }
-          }
+
+
+
+              println
+              println("ERROR: bad case option."); -3 }
         }
+        //println(s"err: ${err}")
       }
       println
       println("results: ")
@@ -106,34 +153,30 @@ object MyApp extends App {
       err
     }
 
-    def median2(data: Seq[Int]): Int = {
-      val (lower, upper) = data.sortWith(_ < _).splitAt(data.size/2)
-      if (data.size % 2 == 0) (lower.last +upper.head)/2 else upper.head
-    }
-
-    val rndData = collection.mutable.ArrayBuffer.fill(5000000)(scala.util.Random.nextInt(100))
-
-    median(new DataNode(false, rndData.toArray))
-
+    val rndData = collection.mutable.ArrayBuffer.fill(5000000)(scala.util.Random.nextInt(1000))
+    median(rndData.toArray)
   }
 
-  //Median
+  Median
 
 
-
+  /*
   object MedianRecurse {
     import scala.collection.mutable._
 
-    case class DataNode(isSorted: Boolean, var data: Array[Int])
-    val max_data_val = 100
+    case class DataNode(isSorted:Boolean, data:Array[Int]) {
+      def printNode = println(s"\nisSorted: ${isSorted}\ndata: ${data.toList}")
+    }
+    case class InfoNode(lb:Int, ub:Int, medianVals:List[Int], medianOffsets:List[Int]) {
+      def printInfo() = println(s"\nmedianVals: ${medianVals.toList}\noffsets: ${medianOffsets.toList}")
+    }
 
 
-
-    def partitionOnEq(k: Int, data: Array[Int]): ArrayBuffer[DataNode] = {
+    def partitionOnEq(k: Int, data: Array[Int]): Array[DataNode] = {
       type abi = ArrayBuffer[Int]
       data match {
-        case v if (v.isEmpty) => ArrayBuffer.empty[DataNode]
-        case v => {
+        case v if (v.isEmpty) => Array.empty[DataNode]
+        case v if (!v.isEmpty) => {
           val res = data.foldLeft((new abi, new abi, new abi)) {
             case ((acl:abi, ace:abi, acg:abi), i) =>
               if (i == k)       (acl, ace :+ i, acg)
@@ -141,96 +184,78 @@ object MyApp extends App {
               else              (acl, ace, acg :+ i)
           }
           val ret = ArrayBuffer.empty[DataNode]
-          if (0 < res._1.length)
-            ret.append(new DataNode(res._1.length == 1, res._1.toArray))
-          if (0 < res._2.length)
-            ret.append(new DataNode(true, res._2.toArray))
-          if (0 < res._3.length)
-            ret.append(new DataNode(res._3.length == 1, res._3.toArray))
-          ret
+          ret.append(new DataNode(res._1.length <= 1, res._1.toArray))
+          ret.append(new DataNode(true, res._2.toArray))
+          ret.append(new DataNode(res._3.length <= 1, res._3.toArray))
+          ret.toArray
         }
       }
     }
 
-    def getNodeIndex(offset:Int, nodes:ArrayBuffer[DataNode]): Int = {
-      var indexCpy = offset
+    def getNodeIndex(offset:Int, nodes:Array[DataNode]): (Int, Int) = {
+      var offsetCpy = offset
       val datalen = nodes.length
-      if (datalen <= 0) { println("getMedianNodeIndex ERROR: no data nodes."); -1 } else {
+      if (datalen <= 0) { println("getMedianNodeIndex ERROR: no data nodes."); (-1, -1) } else {
         var index = 0
-        while (index < datalen && nodes(index).data.length <= indexCpy) {
-          indexCpy -= nodes(index).data.length
+        while (index < datalen && nodes(index).data.length <= offsetCpy) {
+          offsetCpy -= nodes(index).data.length
           index += 1
         }
-        index
+        (index, offsetCpy)
       }
     }
 
-    def medianInit(lb:Int, ub:Int, initNode: DataNode): Int = {
+    def median(data: Array[Int]): Int = {
 
-      val medList = initNode.data.length/2 :: (if (initNode.data.length % 2 == 0) initNode.data.length/2 -1 :: Nil else Nil)
-
-      def recurse(medianOffsets:List[Int], node: DataNode): Int  = {
+      def recurse(info:InfoNode, node:DataNode): Int  = {
 
         println
         println("recurse:")
-        println("medianOffsets: " +medianOffsets)
-        println("data: " +node.isSorted +", " +node.data.toList)
+        info.printInfo
+        node.printNode
 
+        //NO DATA
+        if (node.data.length == 0){
+          println("ERROR: data length 0.")
+          -1
+        }
         //SORTED
-        if (node.isSorted) {
+        else if (node.isSorted) {
           //println("sorted:")
           //medianOffsets.foreach(offset => println(offset.toString))
 
-          //val sum = medianOffsets.foldLeft(0)((ac, i) => {println(s"node.data(${i}): ${node.data(i)}"); ac +node.data(i)});println("sum: " +sum.toString)
-          val sum = medianOffsets.foldLeft(0)((ac, i) => ac +node.data(i))
-          //medianOffsets.foldLeft(0)((ac, i) => ac + node.data(i)) / medianOffsets.length
-          //println("median: " +(sum/medianOffsets.length))
-          //sum/medianOffsets.length
-          sum/medianOffsets.length
+          if (0 < node.data.length) {
+          }
+
+
+
+          info.medianOffsets.foldLeft(0)((ac, index) => ac +node.data(index))/info.medianOffsets.length
         }
         //UNSORTED
         else {
-          val medium = MedianRecurse.max_data_val/2
-          val pivot = (0 until node.data.length/3).foldLeft(node.data(0))((ac, i) => if (node.data(i) -medium <= ac -medium) node.data(i) else ac);println("pivot: " +pivot)
-          val parts = partitionOnEq(pivot, node.data);println("parts: ");parts.foreach(dn => println(dn.data.toList))
 
-          if (medianOffsets.length == 1) {
-            val nodeIndex = getNodeIndex(medianOffsets(0), parts);//println("1 nodeIndex: " +nodeIndex.toString)
-            val dif = (0 until nodeIndex).foldLeft(0)((ac, index) =>  ac +parts(index).data.length);//println("dif: " +dif)
-            val ofs = medianOffsets.map(offset => offset -dif);//println("ofs: " +ofs)
-
-            recurse(ofs, parts(nodeIndex))
-          }
-          else if (medianOffsets.length == 2) {
-            val nodeIndex0 = getNodeIndex(medianOffsets(0), parts);//println("2 nodeIndex0: " +nodeIndex0.toString)
-            val dif0 = (0 until nodeIndex0).foldLeft(0)((ac, index) =>  ac +parts(index).data.length);//println("dif0: " +dif0)
-            val ofs0 = medianOffsets(0) -dif0;//println("ofs0: " +ofs0)
-            val nodeIndex1 = getNodeIndex(medianOffsets(1), parts);//println("2 nodeIndex1: " +nodeIndex1)
-
-            if (nodeIndex0 == nodeIndex1) {
-              val ret = recurse(List(ofs0), parts(nodeIndex0))
-              println("ret: " +ret.toString)
-              ret
+          if (!info.medianOffsets.isEmpty) {
+            val pivot = node.data.find(e => info.lb < e && e < info.ub) match {
+              case Some(p: Int) => p
+              case None => { println("ERROR: bad pivot."); -1 }
             }
-            else {
-              val dif1 = (0 until nodeIndex1).foldLeft(0)((ac, index) =>  ac +parts(index).data.length);//println("dif1: " +dif1)
-              val ofs1 = medianOffsets(1) -dif1;//println("ofs1: " +ofs1)
+            println(s"pivot: ${pivot}")
 
-              val r0 = recurse(List(ofs0), parts(nodeIndex0))
-              val r1 = recurse(List(ofs1), parts(nodeIndex1))
-              println(s"(${r0},${r1})")
-
-              (r0 +r1)/2
+            val parts = partitionOnEq(pivot, node.data)
+            val (part_index, med_offset) = getNodeIndex(info.medianOffsets.head, parts)
+            val newMedianOffsets = info.medianOffsets.map(x => x -med_offset)
+            val (lb:Int, ub:Int) = part_index match {
+              case 0 => (info.lb, pivot)
+              case 1 => (pivot, pivot)
+              case 2 => (pivot, info.ub)
             }
+            recurse(new InfoNode(lb, ub, info.medianVals, info.medianOffsets), parts(part_index))
           }
-          else {
-            println(s"ERROR: median index length ${medianOffsets.length}.")
-            -3
-          }
+          else { println("ERROR: no median offsets"); -1 }
         }
       }
-
-      recurse(medList, initNode)
+      val medianOffsets = data.length/2 :: (if (data.length % 2 == 0) data.length/2 -1 :: Nil else Nil)
+      recurse(new InfoNode(Int.MinValue, Int.MaxValue, Nil, medianOffsets), new DataNode(false, data))
     }
 
     val data_size = 100
@@ -238,7 +263,7 @@ object MyApp extends App {
     val upper_bound = 100
     val rndData = collection.mutable.ArrayBuffer.fill(data_size)(scala.util.Random.nextInt(upper_bound))
 
-    medianInit(new DataNode(false, rndData.toArray))
+    median(rndData.toArray)
 
 
     /*
@@ -255,16 +280,7 @@ object MyApp extends App {
 
 
   }
-
-  MedianRecurse
-
-
-
-
-
-
-
-
-
+  */
+  //MedianRecurse
 
 }
